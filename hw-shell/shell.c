9723cc2ee625ce3c41da3100c2c8d06da60392aa
Shengxiang Lin <154602793+reallinshengxiang@users.noreply.github.com>
2024-10-06 19:42:34 -0700

    Update shell.c

--- hw-shell/shell.c
+++ hw-shell/shell.c
@@ -12,36 +12,32 @@
 #include <unistd.h>
 
 #include "tokenizer.h"
+
 struct tokens {
   size_t tokens_length;
   char** tokens;
   size_t buffers_length;
   char** buffers;
 };
-/* Convenience macro to silence compiler warnings about unused function parameters. */
+
 #define unused __attribute__((unused))
 
-/* Whether the shell is connected to an actual terminal or not. */
 bool shell_is_interactive;
-
-/* File descriptor for the shell input */
 int shell_terminal;
-
-/* Terminal mode settings for the shell */
 struct termios shell_tmodes;
-
-/* Process group id for the shell */
 pid_t shell_pgid;
+
+pid_t background_pids[1024];
+int num_background_pids = 0;
 
 int cmd_exit(struct tokens* tokens);
 int cmd_help(struct tokens* tokens);
 int cmd_pwd(struct tokens* tokens);
 int cmd_cd(struct tokens* tokens);
+int cmd_wait(struct tokens* tokens); 
 
-/* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens* tokens);
 
-/* Built-in command struct and lookup table */
 typedef struct fun_desc {
   cmd_fun_t* fun;
   char* cmd;
@@ -52,19 +48,17 @@ fun_desc_t cmd_table[] = {
     {cmd_help, "?", "show this help menu"},
     {cmd_exit, "exit", "exit the command shell"},
     {cmd_pwd, "pwd", "print current working directory"},
-    {cmd_cd, "cd", "change directory"}
+    {cmd_cd, "cd", "change directory"},
+    {cmd_wait, "wait", "wait for all background jobs to finish"}  // 新增命令描述
 };
-/* Prints a helpful description for the given command */
+
 int cmd_help(unused struct tokens* tokens) {
   for (unsigned int i = 0; i < sizeof(cmd_table) / sizeof(fun_desc_t); i++)
     printf("%s - %s\n", cmd_table[i].cmd, cmd_table[i].doc);
   return 1;
 }
 
-/* Exits this shell */
 int cmd_exit(unused struct tokens* tokens) { exit(0); }
-/* Prints the current working directory */
-
 
 int cmd_pwd(unused struct tokens* tokens) {
   char cwd[1024];
@@ -91,9 +85,14 @@ int cmd_cd(struct tokens* tokens) {
   }
   return 1;
 }
+int cmd_wait(unused struct tokens* tokens) {
+  for (int i = 0; i < num_background_pids; i++) {
+    waitpid(background_pids[i], NULL, 0);
+  }
+  //printf("Done waiting\n");
+  return 1;
+}
 
-
-/* Looks up the built-in command, if it exists. */
 int lookup(char cmd[]) {
   for (unsigned int i = 0; i < sizeof(cmd_table) / sizeof(fun_desc_t); i++)
     if (cmd && (strcmp(cmd_table[i].cmd, cmd) == 0))
@@ -101,28 +100,15 @@ int lookup(char cmd[]) {
   return -1;
 }
 
-/* Intialization procedures for this shell */
 void init_shell() {
-  /* Our shell is connected to standard input. */
   shell_terminal = STDIN_FILENO;
-
-  /* Check if we are running interactively */
   shell_is_interactive = isatty(shell_terminal);
 
   if (shell_is_interactive) {
-    /* If the shell is not currently in the foreground, we must pause the shell until it becomes a
-     * foreground process. We use SIGTTIN to pause the shell. When the shell gets moved to the
-     * foreground, we'll receive a SIGCONT. */
     while (tcgetpgrp(shell_terminal) != (shell_pgid = getpgrp()))
       kill(-shell_pgid, SIGTTIN);
-
-    /* Saves the shell's process id */
     shell_pgid = getpid();
-
-    /* Take control of the terminal */
     tcsetpgrp(shell_terminal, shell_pgid);
-
-    /* Save the current termios to a variable, so it can be restored later. */
     tcgetattr(shell_terminal, &shell_tmodes);
   }
 }
@@ -145,76 +131,8 @@ char* search_in_path(const char* program_name) {
     free(path_dup);
     return NULL;
 }
-void tokens_remove(struct tokens* tokens, int start, int count) {
-    memmove(&tokens->tokens[start], &tokens->tokens[start + count],
-            (tokens->tokens_length - start - count) * sizeof(char*));
-    tokens->tokens_length -= count;
-}
-void run_external_command(struct tokens* tokens,bool background) {
-    int stdin_fd = dup(STDIN_FILENO);  
-    int stdout_fd = dup(STDOUT_FILENO); 
-    for (int i = 0; i < tokens_get_length(tokens) - 1; ++i) {
-        char* token = tokens_get_token(tokens, i);
-        if (strcmp(token, ">") == 0) {
-            char* output_file = tokens_get_token(tokens, i + 1);
-            int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (fd < 0) {
-                perror("open");
-                exit(EXIT_FAILURE);
-            }
-            dup2(fd, STDOUT_FILENO); 
-            close(fd); 
-            printf("%d",i);
-            tokens_remove(tokens, i, 2);
-            i--; 
-        } else if (strcmp(token, "<") == 0) {
-            char* input_file = tokens_get_token(tokens, i + 1);
-            int fd = open(input_file, O_RDONLY);
-            if (fd < 0) {
-                perror("open");
-                exit(EXIT_FAILURE);
-            }
-            dup2(fd, STDIN_FILENO); 
-            close(fd); 
-            printf("%d",i);
-            tokens_remove(tokens, i, 2);
-            i--; 
-        }
-    }
 
-    pid_t pid = fork(); 
-    if (pid == 0) {
-        int token_count = tokens_get_length(tokens);
-        char* args[token_count + 1];
-        for (int i = 0; i < token_count; i++) {
-            args[i] = tokens_get_token(tokens, i); 
-        }
-        args[token_count] = NULL;
-        char* program = args[0];
-
-        if (strchr(program, '/') == NULL) {
-            char* full_path = search_in_path(program);
-            if (full_path != NULL) {
-                execv(full_path, args);
-                free(full_path); 
-            }
-        } else {
-            execv(program, args);
-        }
-        perror("execv");
-        exit(EXIT_FAILURE);
-    } else if (pid > 0) {
-        int status;
-        waitpid(pid, &status, 0);
-    } else {
-        perror("fork");
-    }
-    dup2(stdin_fd, STDIN_FILENO);
-    dup2(stdout_fd, STDOUT_FILENO);
-    close(stdin_fd);
-    close(stdout_fd);
-}
-void run_command_with_pipes(struct tokens* tokens) {
+void run_command_with_pipes(struct tokens* tokens, bool background) {
     int num_commands = 0;
     int token_length = tokens_get_length(tokens);
     for (int i = 0; i < token_length; i++) {
@@ -307,53 +225,76 @@ void run_command_with_pipes(struct tokens* tokens) {
             exit(EXIT_FAILURE);
         }
 
+        if (background) {
+            background_pids[num_background_pids++] = pid;
+        }
+
         command_index++; 
     }
     for (int i = 0; i < num_commands - 1; i++) {
         close(pipe_arr[i][0]);
         close(pipe_arr[i][1]);
     }
-    for (int i = 0; i < num_commands; i++) {
-        wait(NULL);
+    if (!background) {
+        for (int i = 0; i < num_commands; i++) {
+            wait(NULL);
+        }
     }
 }
 
 int main(unused int argc, unused char* argv[]) {
   init_shell();
-  signal(SIGINT, SIG_IGN);
-  signal(SIGQUIT, SIG_IGN);
-  signal(SIGTERM, SIG_IGN);
-  signal(SIGSTOP, SIG_IGN);
-  signal(SIGCONT, SIG_IGN);
-  signal(SIGTTIN, SIG_IGN);
-  signal(SIGTTOU, SIG_IGN);
-  static char line[4096];
+  signal(SIGINT,  SIG_IGN);  
+  signal(SIGTSTP, SIG_IGN);  
   int line_num = 0;
-  /* Please only print shell prompts when standard input is not a tty */
-  if (shell_is_interactive)
-    fprintf(stdout, "%d: ", line_num);
-
-  while (fgets(line, 4096, stdin)) {
-    /* Split our line into words. */
-    struct tokens* tokens = tokenize(line);
-
-    /* Find which built-in function to run. */
-    int fundex = lookup(tokens_get_token(tokens, 0));
-
-    if (fundex >= 0) {
-      cmd_table[fundex].fun(tokens);
-    } else {
-      /* REPLACE this to run commands as programs. */
-      run_command_with_pipes(tokens);  // 执行外部命令
-      //fprintf(stdout, "This shell doesn't know how to run programs.\n");
-    }
-
+  while (1) {
+    char* line = NULL;
+    size_t size = 0;
     if (shell_is_interactive)
       /* Please only print shell prompts when standard input is not a tty */
-      fprintf(stdout, "%d: ", ++line_num);
+      fprintf(stdout, "%d: ", line_num++);
+    if (getline(&line, &size, stdin) == -1) {
+      if (feof(stdin)) {
+        exit(0);
+      } else {
+        perror("getline");
+        continue;
+      }
+    }
 
-    /* Clean up memory */
+    line[strcspn(line, "\n")] = 0;
+
+    struct tokens* tokens = tokenize(line);
+    if (tokens == NULL || tokens_get_length(tokens) == 0) {
+      free(line);
+      continue;
+    }
+
+    char* first_token = tokens_get_token(tokens, 0);
+    if (first_token == NULL) {
+      tokens_destroy(tokens);
+      free(line);
+      continue;
+    }
+
+    int cmd_index = lookup(first_token);
+    if (cmd_index >= 0) {
+      cmd_table[cmd_index].fun(tokens);
+    } else {
+      bool background = false;
+      int token_length = tokens_get_length(tokens);
+      if (strcmp(tokens_get_token(tokens, token_length - 1), "&") == 0) {
+        background = true;
+        tokens->tokens[token_length - 1] = NULL; 
+        tokens->tokens_length--; 
+      }
+
+      run_command_with_pipes(tokens, background);
+
+    }
+
     tokens_destroy(tokens);
+    free(line);
   }
 
   return 0;
